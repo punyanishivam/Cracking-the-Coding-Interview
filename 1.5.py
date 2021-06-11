5be06bd92a222e9394765b2b3195596cdae5680b
punyanishivam@ <punyanishivam@gmail.com>
2021-06-11 18:21:18 +0530

    New solutions added

--- 1.5.py
+++ 1.5.py
@@ -1,32 +1,38 @@
-#OneAway
-def oneAway(str1, str2):
-    #OneAwayCheck
-    return abs(len(str1) - len(str2)) <= 1
-    if len(str1) > len(str2):
-        big = str1
-        small = str2
-    else:
-        big = str2
-        small = str1
-    index1 = 0
-    index2 = 0
-    flag = False
-    while(index2 < len(big) and index1 < len(small)):
-        if (big[index1] != small[index2]):
-            if flag:
+# OneAway
+
+def one_away(s1, s2):
+    if len(s1) == len(s2):
+        return one_edit_replace(s1, s2)
+    elif len(s1) + 1 == len(s2):
+        return one_edit_insert(s1, s2)
+    elif len(s1) - 1 == len(s2):
+        return one_edit_insert(s2, s1)
+    return False
+
+
+def one_edit_replace(s1, s2):
+    edited = False
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            if edited:
                 return False
-            flag = True
-            if len(big) == len(small):
-                index1 += 1
-        else:
-            index1 += 1
-        index2 += 1
+            edited = True
     return True
-        
 
 
-#OneAwayTester
-str1 = "pale"
-str2 = "pdfe"
-result = oneAway(str1, str2)
-print(result)
+def one_edit_insert(s1, s2):
+    edited = False
+    i, j = 0, 0
+    while i < len(s1) and j < len(s2):
+        if s1[i] != s2[j]:
+            if edited:
+                return False
+            edited = True
+            j += 1
+        else:
+            i += 1
+            j += 1
+    return True
+
+
+print(one_away("pale", "bake"))
--- 1.6.py
+++ 1.6.py
@@ -1,12 +1,19 @@
-#String compression
-inputStr = "aabcccccaaa"
-count = 1
-result = ""
-for i in range(len(inputStr) - 1):
-    if inputStr[i] == inputStr[i+1]:
-        count += 1
-    else:
-        result += inputStr[i] + str(count)
-        count = 1
-result += inputStr[-1] + str(count)
-print(result)
+# String compression
+
+def string_compression(input_str):      # Time: O(N)  Space: O(N)
+
+    count = 1
+    result = []
+
+    for i in range(len(input_str) - 1):
+        if input_str[i] == input_str[i+1]:
+            count += 1
+        else:
+            result.append(input_str[i] + str(count))
+            count = 1
+
+    result.append(input_str[-1] + str(count))
+    return "".join(result)
+
+
+print(string_compression("aabcccccaa"))
